5e5768ba5456002950a814baa3e018ccae57be93
Weng <97745870+Wesilau@users.noreply.github.com>
2024-12-12 09:43:33 +0800

    feat: add random showup

--- EyeProtectPet.py
+++ EyeProtectPet.py
@@ -8,13 +8,16 @@
 '''
 
 from PyQt5.QtGui import *
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 import sys
+import os
+import random
+import math
 
 class Pet(object):
     def __init__(self, width=800, height=400):
-        self.image = 'images/xiaogou/xiaogou_0.png'  # 使用静态图像
+        self.image = 'images/xiaogou/xiaogou_0.png'  # 导入图像
         self.rect_x = width
         self.rect_y = height
 
@@ -27,6 +30,17 @@ class App(QWidget):
         self.lb_pet = QLabel(self)  # 创建标签来显示宠物图像
 
         self.init_ui()
+
+        # 创建定时器，每 1 秒调用一次 move_pet 方法
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.move_pet)
+        self.timer.start(1000)
+
+        # 创建一个定时器，10 秒后关闭窗口
+        self.close_timer = QTimer(self)
+        self.close_timer.timeout.connect(self.close_app)
+        self.close_timer.setSingleShot(True)  # 只触发一次
+        self.close_timer.start(10000)  # 10 秒后触发
 
     def init_ui(self):
         # 设置窗口为全屏
@@ -42,6 +56,35 @@ class App(QWidget):
         self.setAutoFillBackground(False)
         self.setAttribute(Qt.WA_TranslucentBackground, True)
         self.showMaximized()
+
+
+    def move_pet(self):
+        # 随机生成新的位置
+        screen = QDesktopWidget().screenGeometry()
+        new_x = random.randint(0, screen.width() - self.pm_pet.width())
+        new_y = random.randint(0, screen.height() - self.pm_pet.height())
+
+        # 移动宠物标签到新位置
+        self.lb_pet.move(new_x, new_y)
+
+    '''
+    def move_pet(self):
+        # 随机生成新的目标位置
+        screen = QDesktopWidget().screenGeometry()
+        new_x = random.randint(0, screen.width() - self.pm_pet.width())
+        new_y = random.randint(0, screen.height() - self.pm_pet.height())
+
+        # 使用动画将标签平滑移动到新位置
+        self.animation = QPropertyAnimation(self.lb_pet, b"pos")
+        self.animation.setDuration(2000)  # 动画持续时间为 2 秒
+        self.animation.setStartValue(self.lb_pet.pos())
+        self.animation.setEndValue(QPoint(new_x, new_y))
+        self.animation.start()
+    '''
+
+    def close_app(self):
+        # 关闭窗口
+        self.close()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
