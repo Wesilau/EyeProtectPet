86192cdb769e99e922db136f477b5d8c263e802b
Wesilau <1257717527@qq.com>
2024-12-13 15:59:32 +0800

    Feat: dynamic pet

--- EyeProtectPet.py
+++ EyeProtectPet.py
@@ -16,10 +16,23 @@ import random
 import math
 
 class Pet(object):
-    def __init__(self, width=800, height=400):
-        self.image = 'images/xiaogou/xiaogou_0.png'  # 导入图像
+    def __init__(self, width=400, height=400):
+        # self.image = 'images/xiaogou/xiaogou_0.png'  # 导入图像
+        self.image_url = 'images/xiaogou/xiaogou_'
+        self.image_key = 1
+        self.image = self.image_url + str(self.image_key) + '.png'
+
         self.rect_x = width
         self.rect_y = height
+        self.image_num = 23  # 图片数量
+
+    def gif(self):
+        if self.image_key < self.image_num - 1:
+            self.image_key += 1
+        else:
+            self.image_key = 1
+        self.image = self.image_url + str(self.image_key) + '.png'
+
 
 class App(QWidget):
     def __init__(self):
@@ -31,16 +44,28 @@ class App(QWidget):
 
         self.init_ui()
 
-        # 创建定时器，每 1 秒调用一次 move_pet 方法
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.move_pet)
-        self.timer.start(1000)
+        # 创建定时器，调用 move_pet 方法
+        self.move_timer = QTimer(self)
+        self.move_timer.timeout.connect(self.move_pet)
+        self.move_timer.start((self.pet.image_num + 1) * 100)
 
         # 创建一个定时器，10 秒后关闭窗口
         self.close_timer = QTimer(self)
         self.close_timer.timeout.connect(self.close_app)
         self.close_timer.setSingleShot(True)  # 只触发一次
-        self.close_timer.start(10000)  # 10 秒后触发
+        self.close_timer.start((self.pet.image_num + 1) * 100 * 8)  # 10 秒后触发
+
+        # 创建一个定时器，0.1 秒更新图片实现gif效果
+        self.gif_timer = QTimer(self)
+        self.gif_timer.timeout.connect(self.gem)
+        self.gif_timer.start(100)
+
+    def gem(self):
+        # 宠物实现gif效果
+        self.pet.gif()
+        self.pm_pet = QPixmap(self.pet.image)
+        self.lb_pet.setPixmap(self.pm_pet)
+        pass
 
     def init_ui(self):
         # 设置窗口为全屏
@@ -57,29 +82,74 @@ class App(QWidget):
         self.setAttribute(Qt.WA_TranslucentBackground, True)
         self.showMaximized()
 
+        # 记录上次宠物所在的区域
+        self.last_area = None
+
 
     def move_pet(self):
         # 随机生成新的位置
         screen = QDesktopWidget().screenGeometry()
-        new_x = random.randint(0, screen.width() - self.pm_pet.width())
-        new_y = random.randint(0, screen.height() - self.pm_pet.height())
+
+        # 将屏幕分成四个区域
+        width = screen.width() // 2
+        height = screen.height() // 2
+
+        areas = [(0, 0, width, height),  # 上左
+                 (width, 0, width, height),  # 上右
+                 (0, height, width, height),  # 下左
+                 (width, height, width, height)]  # 下右
+
+        # 随机选择一个区域，但不能是上次的区域
+        available_areas = [area for area in areas if area != self.last_area]
+        new_area = random.choice(available_areas)
+
+        # 获取新的随机位置
+        new_x = random.randint(new_area[0], new_area[0] + new_area[2] - self.pm_pet.width())
+        new_y = random.randint(new_area[1], new_area[1] + new_area[3] - self.pm_pet.height())
+
+        # new_x = random.randint(0, screen.width() - self.pm_pet.width())
+        # new_y = random.randint(0, screen.height() - self.pm_pet.height())
 
         # 移动宠物标签到新位置
         self.lb_pet.move(new_x, new_y)
 
+        # 记录当前区域
+        self.last_area = new_area
+
     '''
     def move_pet(self):
-        # 随机生成新的目标位置
         screen = QDesktopWidget().screenGeometry()
-        new_x = random.randint(0, screen.width() - self.pm_pet.width())
-        new_y = random.randint(0, screen.height() - self.pm_pet.height())
 
-        # 使用动画将标签平滑移动到新位置
+        # 将屏幕分成四个区域
+        width = screen.width() // 2
+        height = screen.height() // 2
+
+        areas = [(0, 0, width, height),  # 上左
+                 (width, 0, width, height),  # 上右
+                 (0, height, width, height),  # 下左
+                 (width, height, width, height)]  # 下右
+
+        # 随机选择一个区域，但不能是上次的区域
+        available_areas = [area for area in areas if area != self.last_area]
+        new_area = random.choice(available_areas)
+
+        # 获取新的随机位置
+        new_x = random.randint(new_area[0], new_area[0] + new_area[2] - self.pm_pet.width())
+        new_y = random.randint(new_area[1], new_area[1] + new_area[3] - self.pm_pet.height())
+
+        # 使用动画平滑移动宠物
         self.animation = QPropertyAnimation(self.lb_pet, b"pos")
-        self.animation.setDuration(2000)  # 动画持续时间为 2 秒
-        self.animation.setStartValue(self.lb_pet.pos())
-        self.animation.setEndValue(QPoint(new_x, new_y))
-        self.animation.start()
+        self.animation.setDuration(1000)  # 动画持续时间为 1 秒
+        self.animation.setStartValue(self.lb_pet.pos())  # 设置动画起始位置
+        self.animation.setEndValue(QPoint(new_x, new_y))  # 设置动画结束位置
+        self.animation.start()  # 启动动画
+
+        # 更新宠物当前坐标
+        self.pet.rect_x = new_x
+        self.pet.rect_y = new_y
+
+        # 记录当前区域
+        self.last_area = new_area
     '''
 
     def close_app(self):
